e5ee26de9f3a9f686921c8f4b9988f50bbeab180
Kevin Suen <kevinsuen11@gmail.com>
2019-08-05 19:45:38 +0000

    add body parser

--- express_server.js
+++ express_server.js
@@ -1,14 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+// ### Middleware ###
+app.use(bodyParser.urlencoded({extended: true}));
+
+// ### Views to Render ###
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -35,6 +40,7 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+// ### Server listen ###
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
